13190e81134c26b2908ce2583f6a7ef6bafa8b0a
David Potsiadlo <davidpots@gmail.com>
2019-03-31 17:03:49 -0500

    Adds it bruh

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,119 @@
-$(document).ready(function(){
+// String.prototype.splice = function(idx, rem, str) {
+//     return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
+// };
 
+// var captionInput;
+// var captionLength;
+// var banana;
+
+// var brRegex = /<br\s*[\/]?>/gi;
+
+        // function copy() {
+        //   var copyText = document.querySelector("#caption-input");
+        //   copyText.select();
+        //   document.execCommand("copy");
+        // }
+
+$(document).ready(function(){
+    $('#convert').click(function(){
+
+        // Preserve line breaks via invisible character
+        var someText = $('#caption-input').val();
+        someText = someText.replace(/(\r\n|\n|\r)/gm, '\u2800\r\n');
+        $('#caption-input').val(someText);
+        console.log("Input parsed!");
+
+        // Copy to clipboard!
+        $('#caption-input').select()
+        document.execCommand("copy");
+        console.log("Copied to clipboard!")
+        $('body').append('<em>copied to clipboard!</em>');
+
+        return false;
+    });
 });
+
+    //
+    // var hiddenField = document.getElementById("hiddenInput");
+    // hiddenField.value = captionInput
+    //
+    // // hiddenField.value = $('captionInput').innerHTML;
+    // hiddenField.select();
+    // document.execCommand("copy");
+
+        // document.querySelector("#caption-input").select();
+        // $('#caption-input').select();
+        // document.execCommand("copy");
+        // var copyText = document.querySelector("#caption-input");
+        // copyText[0].select();
+        // document.execCommand("copy");
+
+
+
+
+
+
+  // This works! now figure out how to equate them with line breaks b/c this is just doing a single space
+  // someText = someText.replace(/(\r\n|\n|\r)/gm, '\u2800');
+
+
+
+
+
+
+
+
+
+// console.log("===================");
+// console.log($('#caption-input').val().length);
+
+// function parseInput(){
+//   captionInput = $('#caption-input').html();
+//   captionLength = captionInput.length;
+//   console.log(captionInput);
+//   console.log(captionLength);
+// }
+
+// var captionLength = $(captionInput).type();
+
+// console.log("cat");
+
+    // sentence.charCodeAt(index)
+// console.log("asdd");
+
+
+
+
+    // parseInput();
+
+// captionInput.replace(brRegex, "XXX");
+
+// var mystring = "hello world test world";
+// var mystring = captionInput;
+// var find = "&#x2800;";
+// var regex = new RegExp(find, "g");
+// $('#caption-input').html(mystring.replace(regex, "<span>g</span>"));
+
+
+
+
+// "hello yay test yay"
+
+  // parseInput();
+  // var numElements = 0;
+// for (var i = 0; i < 20; i++) {
+//   parseInput();
+//   if ( captionInput.charCodeAt(i) == 103 ) {
+//     console.log(i);
+//     var newTextInput = captionInput.slice(0, i) + "<span>" + captionInput.slice(i, i+1) + "</span>" + captionInput.slice(i+1);
+//     $('#caption-input').html(newTextInput);
+//     // captionInput = $('#caption-input').html();
+//     numElements++;
+//
+//
+//   }
+
+
+
+
+// console.log(newTextInput);
